fe6ed0e6a408b1b8e69c279faeeb6eccfa1a3610
Elsabet <elsabetshiferaw1611199@gmail.com>
2024-11-27 11:07:18 +0300

    Config tailwind css

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,87 +5,87 @@ import DashHead from '../../components/dashHead/DashHead'
 
 function Dashboard() {
   return (
-    <div className="dashboard-container">
-      <Sidebar/>
+    <div className="dashboard-container bg-slate-300">
+      <Sidebar />
       <main className="main-content">
-      <DashHead heading='User Management' />
-      <section className="container-section">
-            <h3>Registered</h3>
-            <div className="table-container">
-                <table className="user-table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Contact</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-            <tr>
-              <td>PR02</td>
-              <td>Tamene Natneal</td>
-              <td>natta22@gmail.com</td>
-              <td>Production Manager</td>
-              <td>0932912092</td>
-            </tr>
-            <tr>
-              <td>IN120</td>
-              <td>Tamene Natneal</td>
-              <td>natta22@gmail.com</td>
-              <td>Inventory Manager</td>
-              <td>0922902072</td>
-            </tr>
-            <tr>
-              <td>EM01</td>
-              <td>Tamene Natneal</td>
-              <td>natta22@gmail.com</td>
-              <td>Employee</td>
-              <td>0903091092</td>
-            </tr>
-            <tr>
-              <td>EM02</td>
-              <td>Tamene Natneal</td>
-              <td>natta22@gmail.com</td>
-              <td>Employee</td>
-              <td>0912239900</td>
-            </tr>
+        <DashHead heading="User Management" />
+        <section className="container-section">
+          <h3>Registered</h3>
+          <div className="table-container">
+            <table className="user-table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Contact</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>PR02</td>
+                  <td>Tamene Natneal</td>
+                  <td>natta22@gmail.com</td>
+                  <td>Production Manager</td>
+                  <td>0932912092</td>
+                </tr>
+                <tr>
+                  <td>IN120</td>
+                  <td>Tamene Natneal</td>
+                  <td>natta22@gmail.com</td>
+                  <td>Inventory Manager</td>
+                  <td>0922902072</td>
+                </tr>
+                <tr>
+                  <td>EM01</td>
+                  <td>Tamene Natneal</td>
+                  <td>natta22@gmail.com</td>
+                  <td>Employee</td>
+                  <td>0903091092</td>
+                </tr>
+                <tr>
+                  <td>EM02</td>
+                  <td>Tamene Natneal</td>
+                  <td>natta22@gmail.com</td>
+                  <td>Employee</td>
+                  <td>0912239900</td>
+                </tr>
 
-            <tr>
-              <td>EM03</td>
-              <td>kidus haile</td>
-              <td>nkidh2@gmail.com</td>
-              <td>Employee</td>
-              <td>0912019900</td>
-            </tr>
+                <tr>
+                  <td>EM03</td>
+                  <td>kidus haile</td>
+                  <td>nkidh2@gmail.com</td>
+                  <td>Employee</td>
+                  <td>0912019900</td>
+                </tr>
 
-            <tr>
-              <td>EM04</td>
-              <td>nahom said</td>
-              <td>saidna00@gmail.com</td>
-              <td>Employee</td>
-              <td>0911019700</td>
-            </tr>
+                <tr>
+                  <td>EM04</td>
+                  <td>nahom said</td>
+                  <td>saidna00@gmail.com</td>
+                  <td>Employee</td>
+                  <td>0911019700</td>
+                </tr>
 
-            <tr>
-              <td>EM05</td>
-              <td>helen telaun</td>
-              <td>nkidh2@gmail.com</td>
-              <td>Employee</td>
-              <td>0912019900</td>
-            </tr>
+                <tr>
+                  <td>EM05</td>
+                  <td>helen telaun</td>
+                  <td>nkidh2@gmail.com</td>
+                  <td>Employee</td>
+                  <td>0912019900</td>
+                </tr>
 
-            <tr>
-              <td>EM06</td>
-              <td>nahom said</td>
-              <td>saidna00@gmail.com</td>
-              <td>Employee</td>
-              <td>0911019700</td>
-            </tr>
-          </tbody>
-                </table>
-            </div>
+                <tr>
+                  <td>EM06</td>
+                  <td>nahom said</td>
+                  <td>saidna00@gmail.com</td>
+                  <td>Employee</td>
+                  <td>0911019700</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </section>
       </main>
     </div>
